5b471c57365dedf6f096acc45b53474aa033f673
pypoh <nafifb@programmer.net>
2020-05-14 23:09:52 +0700

    apip 11.09

--- app/src/main/java/com/example/snapventuremultiplayer/ScoreActivity.kt
+++ app/src/main/java/com/example/snapventuremultiplayer/ScoreActivity.kt
@@ -11,22 +11,25 @@ class ScoreActivity : AppCompatActivity() {
     private val mRef: FirebaseFirestore by lazy { FirebaseFirestore.getInstance() }
     private lateinit var textwin1 : TextView
     private lateinit var textwin2 : TextView
-    private lateinit var scoreplayer1 : String
-    private lateinit var scoreplayer2 : String
+    private var scoreplayer1 : String? = ""
+    private var scoreplayer2 : String? = ""
     //INTENT BUNDLE
-    var extras = intent.extras
-    var roomId = extras!!.getString("roomname")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_score)
 
+        var extras = intent.extras
+        var roomId = extras!!.getString("roomname")
+
         textwin1 = findViewById(R.id.resultwin1)
         textwin2 = findViewById(R.id.resultwin2)
-        textwin1.setText(readDataWin1())
-        textwin2.setText(readDataWin2())
+        textwin1.text = readDataWin1(roomId!!)
+        textwin2.text = readDataWin2(roomId!!)
+
+
     }
 
-    private fun readDataWin1(): String {
+    private fun readDataWin1(roomId: String): String {
         roomId?.let {
             mRef.collection("multiplayer").document(it).get().addOnSuccessListener { doc ->
                 if (doc.exists()) {
@@ -37,10 +40,10 @@ class ScoreActivity : AppCompatActivity() {
                 }
             }
         }
-        return scoreplayer1
+        return scoreplayer1!!
     }
 
-    private fun readDataWin2(): String {
+    private fun readDataWin2(roomId: String): String {
         roomId?.let {
             mRef.collection("multiplayer").document(it).get().addOnSuccessListener { doc ->
                 if (doc.exists()) {
@@ -51,6 +54,6 @@ class ScoreActivity : AppCompatActivity() {
                 }
             }
         }
-        return scoreplayer2
+        return scoreplayer2!!
     }
 }
